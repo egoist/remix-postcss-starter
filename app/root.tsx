3d556a4659b444df05a28b9aef682dd3a228589d
EGOIST <0x142857@gmail.com>
2022-01-11 15:34:54 +0800

    init

---
+++ app/root.tsx
@@ -0,0 +1,44 @@
+import {
+  Links,
+  LinksFunction,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "remix"
+import type { MetaFunction } from "remix"
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "New Remix App",
+  }
+}
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      href: "/build/main.css",
+      rel: "stylesheet",
+    },
+  ]
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="bg-red-500">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  )
+}
